e6395bc1c7007b42b2cd12b653e3693dc97faeab
unknown <renewablezip01@gmail.com>
2022-01-26 21:42:57 +0100

    Add SSR caching

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,24 +1,30 @@
-import {json, useLoaderData } from "remix";
+import {useLoaderData } from "remix";
+const Cache = require('lru-cache');
+
+const ssrCache = new Cache({
+  max: 20,
+  // maxAge: 1000 * 60 * 60, // 1hour
+  maxAge: 5000, // 1hour
+});
+ssrCache.allowStale = true;
 
 export const loader = async ({ params }) => {
-  const js = await fetch("https://timercheck.io/YOURTIMERNAME/60");
-    return json({
-      date: (await js.json()).now
-    },
-    {
-      headers: {
-        "cache-control": "max-age=3600, must-revalidate",
+    const key = "mycache";
+    if (ssrCache.has(key)) {
+      return {
+        date: ssrCache.get(key),
       }
-    })
+    }
+    const js = await fetch("https://timercheck.io/YOURTIMERNAME/60");
+    const time = (await js.json()).now;
+    ssrCache.set(key, time);
+    return {
+      date: time
+    }
 };
 
-export function headers() {
-  return {
-    "cache-control": "max-age=3600, must-revalidate",
-  };
-}
 
 export default function Index() {
   const {date} = useLoaderData();
-  return <h1>Date: {date} |</h1>
+  return <h1>Date: {date}</h1>
 }
