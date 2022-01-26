3263a4ea3f6e9b8df09d9d0e1b74b0d6e454671a
unknown <renewablezip01@gmail.com>
2022-01-26 17:43:02 +0100

    add json

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,14 +2,19 @@ import {json, useLoaderData } from "remix";
 
 export const loader = async ({ params }) => {
   const js = await fetch("https://timercheck.io/YOURTIMERNAME/60");
-    return {
+    return json({
       date: (await js.json()).now
-    }
+    },
+    {
+      headers: {
+        "cache-control": "max-age=3000, s-maxage=36000",
+      }
+    })
 };
 
 export function headers() {
   return {
-    "cache-control": "max-age=3000, s-maxage=36000, stale-while-revalidate=360000",
+    "cache-control": "max-age=3000, s-maxage=36000",
   };
 }
 
